8f51d406c1dbfd09b7945357afcb529726d0fcd8
geekbrains <vika-sann@list.ru>
2021-03-09 18:48:33 +0300

    подключила библиотеку
    подключила библиотеку

---
+++ 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace GeekBrainsAlgos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        }
+    }
+
+    public class BechmarkClass
+    {
+        public int SumValueType(int value)
+        {
+            return 9 + value;
+        }
+
+        public int SumRefType(object value)
+        {
+            return 9 + (int)value;
+        }
+
+        [Benchmark]
+        public void TestSum()
+        {
+            SumValueType(99);
+        }
+
+        [Benchmark]
+        public void TestSumBoxing()
+        {
+            object x = 99;
+            SumRefType(x);
+        }
+    }
+}
